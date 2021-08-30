6b8e1a2a03d1d9fcadfbd20fafb17408826e2778
Camilo Andres Robles Mosalve <camilo9612>
2021-08-30 17:03:22 -0500

    boton hola

--- CapaVistas/Form1Prueba.Designer.cs
+++ CapaVistas/Form1Prueba.Designer.cs
@@ -46,11 +46,11 @@ namespace DSI.CapaVistas
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(342, 181);
+            this.button1.Location = new System.Drawing.Point(355, 195);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
-            this.button1.Text = "Hola";
+            this.button1.Text = "hola";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1Prueba
