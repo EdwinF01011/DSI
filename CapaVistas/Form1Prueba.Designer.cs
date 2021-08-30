e462b880003d0b405393e032c9d80bc49e43e7dd
Camilo Andres Robles Mosalve <camilo9612>
2021-08-30 16:48:32 -0500

    agregue el boton de hola

--- CapaVistas/Form1Prueba.Designer.cs
+++ CapaVistas/Form1Prueba.Designer.cs
@@ -30,24 +30,37 @@ namespace DSI.CapaVistas
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(237, 180);
+            this.label1.Location = new System.Drawing.Point(158, 117);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(183, 32);
+            this.label1.Size = new System.Drawing.Size(120, 24);
             this.label1.TabIndex = 0;
             this.label1.Text = "Probando Git";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(342, 181);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Hola";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form1Prueba
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(533, 292);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1Prueba";
             this.Text = "Form1Prueba";
             this.ResumeLayout(false);
@@ -58,5 +71,6 @@ namespace DSI.CapaVistas
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
